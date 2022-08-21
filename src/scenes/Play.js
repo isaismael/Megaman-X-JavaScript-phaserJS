2eb65f449aecd00c639b7c7e80bc297fa57182a4
Ismael Isa <ismael.isa02@gmail.com>
2022-08-20 23:57:26 -0300

    player y movimientos

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,3 +1,5 @@
+import Player from "../entities/Player.js";
+
 class Play extends Phaser.Scene {
     constructor() {
         super({key: 'Play'});
@@ -8,19 +10,46 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        const map = this.make.tilemap({key:'nivel-1'});
-        const tileset1 = map.addTilesetImage('escenario', 'tiles-1');
-        
-        map.createStaticLayer('platforms', tileset1)
-        const platformsColliders = map.createStaticLayer('platforms_colliders', tileset1)
-        
-        platformsColliders.setCollisionByProperty({collides: true})
-        
+        // creamos const para llamar las funciones que crean el map
+        const map = this.createMap();
+        const layers = this.createLayers(map);
+
+        // Const para llamar a la funcion que crea el player
+        const player = this.createPlayer();
+
+        //Creamos una velocidad para el player
+        this.playerSpeed = 200;
+
+        // Agregar las colisiones al player con los colisiones de las plataformas
+        // Acá decimos que va a colisionar con qué
+        this.physics.add.collider(player, layers.platformsColliders);
+
     }
 
-    update() {
-        
+    // Funcion para llamar el tilmap
+    createMap () {
+        const map = this.make.tilemap({key:'nivel-1'});
+        map.addTilesetImage('escenario', 'tiles-1');
+
+        return map;
     }
+
+    // funcion que pinta los tiles en el juego y agregamos colisiones
+    createLayers(map) {
+        const tileset = map.getTileset('escenario')
+        const platformsColliders = map.createStaticLayer('platforms_colliders', tileset);
+        const platforms = map.createStaticLayer('platforms', tileset);
+
+        platformsColliders.setCollisionByProperty({collides: true})
+
+        return{platforms, platformsColliders}
+    }
+
+    // Funcion para crear al player
+    createPlayer() {
+        return new Player(this, 150, 100);
+    }
+
 }
 
 export default Play;
